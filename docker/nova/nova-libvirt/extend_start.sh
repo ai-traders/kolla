e64b0b67914c2fc7650e2d4f62a98faad4190065
Tomasz Setkowski <tom@ai-traders.com>
2018-03-20 18:36:36 +0000

    fix /dev/kvm permissions, allow gid to change #12641

--- docker/nova/nova-libvirt/extend_start.sh
+++ docker/nova/nova-libvirt/extend_start.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-# TODO(SamYaple): Tweak libvirt.conf rather than change permissions.
-# Fix permissions for libvirt
-# Do not remove unless CentOS has been validated
-if [[ -c /dev/kvm ]]; then
-    chmod 660 /dev/kvm
-    chown root:qemu /dev/kvm
-fi
+AIT_KVM_GID=${AIT_KVM_GID:-113}
+
+groupmod --gid ${AIT_KVM_GID} kvm
 
 # Mount xenfs for libxl to work
 if [[ $(lsmod | grep xenfs) ]]; then
